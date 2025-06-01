2905f69e283e0871c3b7643853af8fef733ba84b
africaabad <161127062+africaabad@users.noreply.github.com>
2025-06-01 18:54:12 +0200

    Update alumno.py

--- alumno.py
+++ alumno.py
@@ -60,18 +60,11 @@ def leeAlumnos(ficAlumnos):
     expr_id = r'\s*(?P<id>\d+)\s+'
     expr_nom = r'(?P<nom>[\w\s]+?)\s+'
     expr_notes = r'(?P<notes>[\d.\s]+)\s*'
-    expresion = re.compile(expr_id + expr_nom + expr_notes) # más manejable
-    # expresion = re.compile(r'\s*\d+\s+[\w\s]+[\d.]+\s*') # r:regular. s:space. d: *:cero o mas veces. +una o mas veces
-    # expresion = re.compile(r'\s*(?P<id>\d+)\s+(?P<nom>[\w\s]+?)\s+(?P<nota>[\d.\s]+)\s*') # r:regular. s:space. d: *:cero o mas veces. +una o mas veces
-    # abrir un archivo con gestor de contenido
+    expresion = re.compile(expr_id + expr_nom + expr_notes) 
     with open(ficAlumnos, 'rt', encoding='utf-8') as fpAlumnos: 
         for linea in fpAlumnos:
             match = expresion.match(linea.strip())
-            # match = expresion.search(linea)
             if match is not None: 
-                # id = int(match.group(1))
-                # nom = match.group(2).strip()
-                # notes = list(map(float, re.split(r'\s+', match.group(3).strip())))
                 id = int(match['id'])
                 nom = match['nom']
                 notes = [float(nota) for nota in match['notes'].split()]
